75d3683531792f54003470e061a93a8a75e9f563
gochanghai <gochanghai@aliyun.com>
2019-11-03 02:08:05 +0800

    添加 Postgres 连接

--- go-gin+gorm/init/mysql.go
+++ go-gin+gorm/init/mysql.go
@@ -6,19 +6,33 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-var Eloquent *gorm.DB
+var db *gorm.DB
 
 // mysql 数据库初始化
 func init() {
 	var err error
 	// root:root ==>> 用户名:密码
-	Eloquent, err = gorm.Open("mysql", "root:root@tcp(www.changhai.com:3306)/test?charset=utf8&parseTime=True&loc=Local&timeout=10ms")
+	db, err = gorm.Open("mysql", "root:root@tcp(www.changhai.com:3306)/test?charset=utf8&parseTime=True&loc=Local&timeout=10ms")
 
 	if err != nil {
 		fmt.Printf("mysql connect error %v", err)
 	}
 
-	if Eloquent.Error != nil {
-		fmt.Printf("database error %v", Eloquent.Error)
+	if db.Error != nil {
+		fmt.Printf("database error %v", db.Error)
+	}
+}
+
+// mysql 数据库初始化
+func initPostgres() {
+	var err error
+	db, err = gorm.Open("postgres", "host:www.gochanghai.com dbname=test port=5432  user=root password=root  charset=utf8 parseTime=True loc=Local timeout=10ms")
+
+	if err != nil {
+		fmt.Printf("postgres connect error %v", err)
+	}
+
+	if db.Error != nil {
+		fmt.Printf("database error %v", db.Error)
 	}
 }
