cf9114870693a699cb83fdecf8ae203ef8410669
gochanghai <gochanghai@aliyun.com>
2019-11-10 23:39:57 +0800

    添加 go 操作 redis

---
+++ go-redis/init/initRedis.go
@@ -0,0 +1,41 @@
+package init
+
+import (
+	"flag"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+//声明一些全局变量
+var (
+	pool          *redis.Pool
+	redisServer   = flag.String("redisServer", "www.gochanghai.com:6379", "")
+	redisPassword = flag.String("redisPassword", "lch2199.", "")
+)
+
+//初始化一个pool
+func initPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3, /*最大的空闲连接数*/
+		MaxActive:   5, /*最大的激活连接数*/
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			if _, err := c.Do("AUTH", password); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
