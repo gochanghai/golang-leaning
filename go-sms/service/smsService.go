85b38245df6ca27646950bf23bc308bf0a901921
gochanghai <gochanghai@aliyun.com>
2019-11-16 23:20:48 +0800

    添加

--- go-sms/service/smsService.go
+++ go-sms/service/smsService.go
@@ -1,5 +1,11 @@
 package service
 
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
 const (
 	mnsDomain = "1943695596114318.mns.cn-hangzhou.aliyuncs.com"
 )
@@ -10,6 +16,7 @@ type SmsService struct {
 
 //发送短信验证码
 func (smsService SmsService) SendSmsCode(phone string) bool {
-
+	code := rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000)
+	strCode := string(code)
 	return true
 }
