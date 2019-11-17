7e61a0d811b65ae65c15c2bada1579baa4d7d57b
gochanghai <gochanghai@aliyun.com>
2019-11-17 23:54:31 +0800

    更新

--- go-sms/service/smsService.go
+++ go-sms/service/smsService.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/endpoints"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/dybaseapi"
 	"math/rand"
 	"strconv"
 	"time"
@@ -10,6 +12,7 @@ const (
 	mnsDomain = "1943695596114318.mns.cn-hangzhou.aliyuncs.com"
 )
 
+
 /* 定义结构体 */
 type SmsService struct {
 }
@@ -18,5 +21,23 @@ type SmsService struct {
 func (smsService SmsService) SendSmsCode(phone string) bool {
 	code := rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000)
 	strCode := string(code)
+
+	endpoints.AddEndpointMapping("cn-hangzhou", "Dybaseapi", "dybaseapi.aliyuncs.com")
+
+	// 创建client实例
+	client, err := dybaseapi.NewClientWithAccessKey(
+		"cn-hangzhou",           // 您的可用区ID
+		"<AccessKeyId>",         // 您的Access Key ID
+		"<AccessKeySecret>")     // 您的Access Key Secret
+	if err != nil {
+		// 异常处理
+		panic(err)
+	}
+
+	queueName := "<QueueName>"
+	messageType := "<MessageType>"
+
+	var token *dybaseapi.MessageTokenDTO
+
 	return true
 }
